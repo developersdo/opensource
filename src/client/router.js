5d1e2b1a258a1a68734693430d874e6873b39f11
Rubens Mariuzzo <rubens@mariuzzo.com>
2017-06-28 00:18:27 -0400

    Work around for GitHub pages.

--- src/client/router.js
+++ src/client/router.js
@@ -1,5 +1,7 @@
 import home from './pages/home'
 
+const base = process.env.NODE_ENV === 'development' ? '' : '/opensource'
+
 export default (app) => {
-  app.route('/', home)
+  app.route(`${base}/`, home)
 }
