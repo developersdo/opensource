fe6a7234a460ec3c453279290aef651b567cc346
Danny Feliz <dannyfeliz08@gmail.com>
2017-10-01 14:39:44 -0400

    Add thousands separator to stats

--- src/client/pages/home/components/stats/index.js
+++ src/client/pages/home/components/stats/index.js
@@ -5,32 +5,32 @@ import styles from './styles.scss'
 
 export default (state, emit) => {
 
-  // Count langaues for each repos.
-  const langStats = state.repos.reduce((stats, repo) => {
-    const langs = repo.languages
-      .split(' ')
-      .filter((lang) => lang)
+    // Count langaues for each repos.
+    const langStats = state.repos.reduce((stats, repo) => {
+        const langs = repo.languages
+            .split(' ')
+            .filter((lang) => lang)
 
-    langs.forEach((lang) => {
-      if (!stats[lang]) {
-        stats[lang] = 0
-      }
-      stats[lang]++
-    })
-    return stats
-  }, {})
+        langs.forEach((lang) => {
+            if (!stats[lang]) {
+                stats[lang] = 0
+            }
+            stats[lang]++
+        })
+        return stats
+    }, {})
 
-  const langs = Object.keys(langStats)
-    .map((name) => {
-      return {
-        name,
-        total: langStats[name],
-        percentage: ((langStats[name] / state.repos.length) * 100).toFixed(2)
-      }
-    })
-    .sort((a, b) => b.total - a.total)
+    const langs = Object.keys(langStats)
+        .map((name) => {
+            return {
+                name,
+                total: langStats[name],
+                percentage: ((langStats[name] / state.repos.length) * 100).toFixed(2)
+            }
+        })
+        .sort((a, b) => b.total - a.total)
 
-  return html`
+    return html `
     <div>
       <div class="row">
         <h3>📊 Stats</h3>
@@ -44,17 +44,17 @@ export default (state, emit) => {
     </div>
   `
 
-  function renderLangStats(langs) {
-    langs = langs.slice(0, 12)
-    return html`
+    function renderLangStats(langs) {
+        langs = langs.slice(0, 12)
+        return html `
       <div>
-        <h4>Popular Languages <small>(across ${ state.repos.length } repos)</small></h4>
+        <h4>Popular Languages <small>(across ${ state.repos.length.toLocaleString() } repos)</small></h4>
         ${ langs.map((lang) => {
           return html`
             <div>
               <span class="${ styles.name }">${ lang.name }</span>
               <strong>${ lang.percentage }%</strong>
-              (${ lang.total } repos)
+              (${ lang.total.toLocaleString() } repos)
               <div class="progress ${ styles.progress }">
                 <div class="determinate" style="width: ${ lang.percentage }%"></div>
               </div>
@@ -80,17 +80,17 @@ export default (state, emit) => {
         <h4>Some Interesting Numbers</h4>
         <ul class="collection">
           <li class="collection-item">
-            <h4><i class="material-icons">perm_identity</i> ${ state.users.length } users <small>are contributing to open source.</small></h4>
+            <h4><i class="material-icons">perm_identity</i> ${ state.users.length.toLocaleString() } users <small>are contributing to open source.</small></h4>
             <p>(organizations are not counted)</p>
-            <p>Among that number of users, <strong>${ usersWithMoreThanTenRepo } have more 10 repos</strong>.</p>
+            <p>Among that number of users, <strong>${ usersWithMoreThanTenRepo.toLocaleString() } have more 10 repos</strong>.</p>
           </li>
           <li class="collection-item">
-            <h4><i class="material-icons">code</i> ${ state.repos.length } repos <small>created on GitHub. In average, each user contribute with ${ (state.repos.length / state.users.length).toFixed(1) } repos.</small></h4>
+            <h4><i class="material-icons">code</i> ${ state.repos.length.toLocaleString() } repos <small>created on GitHub. In average, each user contribute with ${ (state.repos.length / state.users.length).toFixed(1).toLocaleString() } repos.</small></h4>
             <p>(excluding forks)</p>
-            <p>Interesting, the fact that there are <strong>${ reposWithMoreThanOneStargazer } repos with more than one star</strong>.</p>
+            <p>Interesting, the fact that there are <strong>${ reposWithMoreThanOneStargazer.toLocaleString() } repos with more than one star</strong>.</p>
           </li>
           <li class="collection-item">
-            <h4><i class="material-icons">assessment</i> ${ langs.length } programming languages <small>in use across ${ state.repos.length } repos.</small></h4>
+            <h4><i class="material-icons">assessment</i> ${ langs.length.toLocaleString() } programming languages <small>in use across ${ state.repos.length.toLocaleString() } repos.</small></h4>
             <p>The <strong>less used languages</strong> among dominican developers in open source via GitHub are: ${ langs.slice(-10).map((lang) => lang.name).join(', ') }.</p>
           </li>
         </ul>
