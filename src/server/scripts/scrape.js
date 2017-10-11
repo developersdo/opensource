e08410ccf3866ac7a3fe7ff2ddf5aba0d1f41d69
Patrick Rainier Juen <patrickrainier@gmail.com>
2017-10-11 12:44:59 +0800

    Use minimist when parsing arguments

--- src/server/scripts/scrape.js
+++ src/server/scripts/scrape.js
@@ -1,22 +1,20 @@
 #!/usr/bin/env node
 
-const args = process.argv.slice(2)
-console.log(process.argv)
+const args = require('minimist')(process.argv.slice(2))
 const parameters = {
-	['only']: {
+	only: {
 		user: require('../services/scraper/users'),
 		repo: require('../services/scraper/repos')
 	}
 }
 
-console.log('arg', args)
-if (args.length === 0) {
+const key = Object.keys(args)[1]
+if (!key) {
 	require('../services/scraper').scrape()
 }
 else {
-	const [param,option] = args[0].split('=')
-	const key = parameters[param] || {}
-	const fn = key[option]
+	const param = parameters[key] || {}
+	const fn = param[args[key]]
 	if (fn) fn.scrape()
 	else console.log('Invalid arguments')
 }
