f6a58967659e676f7fa6a8a443f137a01f965985
Vijay Krish <vijayakrishna@razorthink.com>
2017-10-13 10:44:41 +0530

    review changes done

--- src/server/scripts/scraper.js
+++ src/server/scripts/scraper.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
 
-//destructing the command line arguments
-const [,,...args]=process.argv;
-
-switch(args[0]){
+//destructing the command line argumentsn
+const [,,...args]= process.argv
+ 
+switch(args[0]) {
 	case '--only=users':
-	  console.log('fetching only users repos ===============');	
-	  require('../services/scraper/users').scrape();
+	  require('../services/scraper/users').scrape()
 	break;
 	case '--only=repos':
-	  console.log('fetching only repos ===============');	
-	  require('../services/scraper/repos').scrape();
+	  require('../services/scraper/repos').scrape()
 	break;
 	default:
-	  console.log("fetching all repos ===============");	
-	  require('../services/scraper').scrape();	
+	  require('../services/scraper').scrape();
 
 }
 
