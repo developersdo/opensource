985d29b54bef54636f16a60d1631d1af2f22cd9e
Rubens Mariuzzo <rubens@mariuzzo.com>
2017-10-15 20:12:10 -0400

    Added SubNavLink component.

--- src/client/components/developers/Developers.js
+++ src/client/components/developers/Developers.js
@@ -2,16 +2,13 @@ import React from 'react'
 import { Route, Redirect, NavLink } from 'react-router-dom'
 import Popular from './popular/Popular'
 import RecentlyJoined from './recently-joined/RecentlyJoined'
+import SubNavLink from '../sub-nav-link/SubNavLink'
 
 const Developers = (props) => (
   <div id="developers">
-    <div className="row">
-      <div className="col s2 center-align offset-s4">
-        <NavLink to="/developers/popular" activeStyle={{ textDecoration: 'underline' }}>Popular</NavLink>
-      </div>
-      <div className="col s2 center-align">
-        <NavLink to="/developers/recently-joined" activeStyle={{ textDecoration: 'underline' }}>Recently joined</NavLink>
-      </div>
+    <div className="row center-align">
+      <SubNavLink to="/developers/popular">Popular</SubNavLink>
+      <SubNavLink to="/developers/recently-joined">Recently Joined</SubNavLink>
     </div>
     <Route exact path="/developers" render={() => <Redirect to="/developers/popular" />} />
     <Route path="/developers/popular" component={Popular} />
--- src/client/components/repositories/Repositories.js
+++ src/client/components/repositories/Repositories.js
@@ -3,19 +3,14 @@ import { Route, Redirect, NavLink } from 'react-router-dom'
 import Popular from './popular/Popular'
 import Trending from './trending/Trending'
 import New from './new/New'
+import SubNavLink from '../sub-nav-link/SubNavLink'
 
 const Repositories = (props) => (
   <div id="repositories">
-    <div className="row">
-      <div className="col s2 center-align offset-s3">
-        <NavLink to="/repositories/popular" activeStyle={{ textDecoration: 'underline' }}>Popular</NavLink>
-      </div>
-      <div className="col s2 center-align">
-        <NavLink to="/repositories/trending" activeStyle={{ textDecoration: 'underline' }}>Trending</NavLink>
-      </div>
-      <div className="col s2 center-align">
-        <NavLink to="/repositories/new" activeStyle={{ textDecoration: 'underline' }}>New</NavLink>
-      </div>
+    <div className="row center-align">
+      <SubNavLink to="/repositories/popular">Popular</SubNavLink>
+      <SubNavLink to="/repositories/trending">Trending</SubNavLink>
+      <SubNavLink to="/repositories/new">New</SubNavLink>
     </div>
     <Route exact path="/repositories" render={() => <Redirect to="/repositories/popular" />} />
     <Route path="/repositories/popular" component={Popular} />
