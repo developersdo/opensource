58f7edd0952094fdc1e8efe13bf0b2dc5dc4b064
Rubens Mariuzzo <rubens@mariuzzo.com>
2017-10-19 00:09:28 -0400

    Added PopularLanguages component to About page.

--- src/client/components/about/About.js
+++ src/client/components/about/About.js
@@ -4,7 +4,13 @@ import Statistics from './statistics/Statistics'
 
 const About = () => (
   <div id="about">
-    <h1>About</h1>
+    <h5>About this website</h5>
+    <p>
+      <strong>Dominican Open Source</strong> is an initiative to gather all possible public data about projects in GitHub from developers of the Dominican Republic using the <a href="https://developer.github.com/v4/" target="_blanks">GitHub GraphQL API</a>.
+    </p>
+    <p>
+      The main purpose of this website is to show local efforts that Dominicans are contributing toward open source communities.
+    </p>
     <div className="row">
       <div className="col s6">
         <PopularLanguages />
--- src/client/components/about/popular-languages/PopularLanguages.js
+++ src/client/components/about/popular-languages/PopularLanguages.js
@@ -1,7 +1,84 @@
 import React from 'react'
+import store from '../../../store/store'
+import Loading from '../../loading/Loading'
+import { map } from 'lodash'
 
-const AboutPopularLanguages = () => (
-  <h4>Popular Languages</h4>
-)
+const style = {
+  position: {
+    width: 40,
+    display: 'inline-block',
+    fontSize: 18,
+  },
+  name: {
+    fontSize: 22,
+    marginRight: 10,
+  },
+  progress: {
+    height: 20,
+  }
+}
+
+class AboutPopularLanguages extends React.Component {
+  state = {
+    repos: [],
+    loading: true,
+    error: false
+  }
+  componentDidMount() {
+    store.getRepos().then((response) => {
+      this.setState({
+        repos: response.items,
+        loading: !response.ready,
+        error: response.error
+      })
+    })
+  }
+  render() {
+    const { repos, loading, error } = this.state
+    if (loading) {
+      return <Loading />
+    }
+
+    // Count languages for each repos.
+    const totals = repos.reduce((total, repo) => {
+      repo.languages.forEach((lang) => {
+        if (!total[lang.name]) {
+          total[lang.name] = 0
+        }
+        total[lang.name]++
+      })
+      return total
+    }, {})
+
+    // Create language stats.
+    const languages = map(totals, (total, name) => ({
+      name,
+      total,
+      percentage: ((total / repos.length) * 100).toFixed(2)
+    }))
+
+    languages.sort((a, b) => b.total - a.total)
+
+    const topLanguages = languages.slice(0, 12)
+
+    console.log(topLanguages)
+
+    return (
+      <div>
+        <h4>Popular Languages</h4>
+        {topLanguages.map(({name, total, percentage}, index) => (
+          <div key={name}>
+            <span className="grey-text text-darken-3" style={style.position}>#{index + 1}</span>
+            <span style={style.name}>{name}</span>
+            <strong className="grey-text text-darken-2">{percentage}%</strong>
+            <div className="progress blue lighten-4" style={style.progress}>
+              <div className="determinate blue darken-2" style={{ width: `${percentage}%` }}></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
 
 export default AboutPopularLanguages
