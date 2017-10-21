852ef751cd24945bd02f4887f102c4891f3fc57a
Zachary Werner <drummerkyran77@gmail.com>
2017-10-20 21:27:11 -0400

    Create repo_changes table (#20)
    * Saves repo history when scraping and a diff is found

--- src/server/services/repo/index.js
+++ src/server/services/repo/index.js
@@ -16,20 +16,45 @@ module.exports = {
     return Promise.all(
       repos.map((repo) => {
         return Repo.findById(repo.id)
-          .then((exist) => {
-            if (exist) {
-              return Repo.update({
-                name: repo.name,
-                description: repo.description,
-                url: repo.url,
-                languages: repo.languages.nodes.map(l => l.name.toLowerCase()).join(' '),
-                stargazers: repo.stargazers.total,
-                watchers: repo.watchers.total,
-                forks: repo.forks.total,
-                createdAt: new Date(repo.createdAt),
-                scrapedAt: new Date(),
-              }, {
-                where: { id: repo.id }
+          .then((existingRepo) => {
+            if (existingRepo) {
+              const languages = repo.languages.nodes.map(l => l.name.toLowerCase()).join(' ');
+              return Promise.resolve().then(function () {
+                if (
+                    existingRepo.name !== repo.name
+                    || existingRepo.description !== repo.description
+                    || existingRepo.url !== repo.url
+                    || existingRepo.languages !== languages
+                    || existingRepo.stargazers !== repo.stargazers.total
+                    || existingRepo.watchers !== repo.watchers.total
+                    || existingRepo.forks !== repo.forks.total
+                ) {
+                  return Repo.create({
+                    repoId: existingRepo.id,
+                    name: existingRepo.name,
+                    description: existingRepo.description,
+                    url: existingRepo.url,
+                    languages: existingRepo.languages,
+                    stargazers: existingRepo.stargazers,
+                    watchers: existingRepo.watchers,
+                    forks: existingRepo.forks,
+                    createdAt: new Date()
+                  })
+                }
+              }).then(function () {
+                return Repo.update({
+                  name: repo.name,
+                  description: repo.description,
+                  url: repo.url,
+                  languages: languages,
+                  stargazers: repo.stargazers.total,
+                  watchers: repo.watchers.total,
+                  forks: repo.forks.total,
+                  createdAt: new Date(repo.createdAt),
+                  scrapedAt: new Date(),
+                }, {
+                  where: { id: repo.id }
+                })
               })
             } else {
               return Repo.create({
