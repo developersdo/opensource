d5affba657736d8b40d4b4e0fabf8d22b897f9f8
Phillip Thomas <pyromaniac931@yahoo.com>
2017-10-21 11:32:06 -0400

    Changed logic to use foreach instead of reduce, Fixed incorrect indexing in Popular.js

--- src/client/components/repositories/popular/Popular.js
+++ src/client/components/repositories/popular/Popular.js
@@ -27,13 +27,11 @@ class PopularRepositories extends React.Component {
 
     const orderedRepos = orderBy(repos, ['stargazers', 'forks', 'watchers', 'name'], ['desc', 'desc', 'desc', 'asc'])
 
-    // Add position to each repos. Some repos will have the same amount of stars, therefore
-    // we have to assign the same position for those cases.
-    let currentPosition = 0
-    orderedRepos.reduce((previous, current) => {
+    // Add position to each repos.
+    let currentPosition = 1
+    _.forEach(orderedRepos, (current) => {
       current.position = currentPosition++
-      return current
-    }, {})
+    })
 
     return <RepositoryList repos={orderedRepos} />
   }
