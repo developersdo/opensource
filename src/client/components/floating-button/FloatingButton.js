5734555b5862cf6b23300e0a5428b3e5e2dc618e
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2018-10-10 21:22:06 -0400

    Migrated to Materialize 1.0

--- src/client/components/floating-button/FloatingButton.js
+++ src/client/components/floating-button/FloatingButton.js
@@ -1,38 +1,48 @@
 import React from 'react'
 import { OutboundLink } from 'react-ga'
 
-const FloatingButton = () => (
-  <div className="fixed-action-btn">
-    <a className="btn-floating btn-large red darken-2">
-      <i className="large material-icons">more_vert</i>
-    </a>
-    <ul>
-      <li>
-        <OutboundLink
-          className="btn-floating red darken-2 tooltipped"
-          target="_blank"
-          eventLabel="https://github.com/developersdo/opensource/issues/new"
-          to="https://github.com/developersdo/opensource/issues/new"
-          data-position="left"
-          data-tooltip="Report an issue"
-        >
-          <i className="material-icons">bug_report</i>
-        </OutboundLink>
-      </li>
-      <li>
-        <OutboundLink
-          className="btn-floating red darken-2 tooltipped"
-          target="_blank"
-          eventLabel="https://github.com/developersdo/opensource/"
-          to="https://github.com/developersdo/opensource/"
-          data-position="left"
-          data-tooltip="View project on GitHub"
-        >
-          <i className="material-icons">code</i>
-        </OutboundLink>
-      </li>
-    </ul>
-  </div>
-)
+class FloatingButton extends React.Component {
+
+  componentDidMount() {
+    const elems = document.querySelectorAll('.fixed-action-btn')
+    window.M.FloatingActionButton.init(elems)
+  }
+
+  render() {
+    return (
+      <div className="fixed-action-btn">
+        <a className="btn-floating btn-large red darken-2">
+          <i className="large material-icons">more_vert</i>
+        </a>
+        <ul>
+          <li>
+            <OutboundLink
+              className="btn-floating red darken-2 tooltipped"
+              target="_blank"
+              eventLabel="https://github.com/developersdo/opensource/issues/new"
+              to="https://github.com/developersdo/opensource/issues/new"
+              data-position="left"
+              data-tooltip="Report an issue"
+            >
+              <i className="material-icons">bug_report</i>
+            </OutboundLink>
+          </li>
+          <li>
+            <OutboundLink
+              className="btn-floating red darken-2 tooltipped"
+              target="_blank"
+              eventLabel="https://github.com/developersdo/opensource/"
+              to="https://github.com/developersdo/opensource/"
+              data-position="left"
+              data-tooltip="View project on GitHub"
+            >
+              <i className="material-icons">code</i>
+            </OutboundLink>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
 
 export default FloatingButton
